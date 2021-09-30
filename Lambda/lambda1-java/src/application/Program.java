f03ab232ae936caf7c7e77732fa0d12bdceb9b70
Renan Luz <ren4n@live.com>
2021-09-30 13:07:24 -0300

    Lambda 1

--- Lambda/lambda1-java/src/application/Program.java
+++ Lambda/lambda1-java/src/application/Program.java
@@ -9,22 +9,21 @@ import entities.Product;
 public class Program {
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.0));
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
-		
-		Comparator<Product> comp = new Comparator<Product>() {
 
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().compareToIgnoreCase(p2.getName());
+
 		};
-		
+
 		list.sort(comp);
-		
-		for(Product p : list) {
+
+		for (
+
+		Product p : list) {
 			System.out.println(p);
 		}
 	}
