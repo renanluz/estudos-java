208660a45ee5c663e5e00ae7becf0bfb91135072
Renan Luz <ren4n@live.com>
2021-09-30 15:03:10 -0300

    Inline lambda expression

--- Lambda/lambda3-java/src/application/Program.java
+++ Lambda/lambda3-java/src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -15,13 +14,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		double factor = 1.8;
+		double factor = 1.1;
 
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * factor);
-		};
-
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
 		list.forEach(System.out::println);
 	}
