16c79fb41a543dddbb81c3fc974800823e3cb90f
Renan Luz <ren4n@live.com>
2021-09-30 13:12:52 -0300

    Lambda 3

--- Lambda/lambda1-java/src/application/Program.java
+++ Lambda/lambda1-java/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -14,13 +13,9 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
 
-		Comparator<Product> comp = (p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName());
+		list.sort((p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName()));
 
-		list.sort(comp);
-
-		for (
-
-		Product p : list) {
+		for (Product p : list) {
 			System.out.println(p);
 		}
 	}
