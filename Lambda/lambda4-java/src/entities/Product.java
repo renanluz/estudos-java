1a93e7486ef81a0fa171a8361b646729986e6989
Renan Luz <ren4n@live.com>
2021-09-30 15:43:58 -0300

    Declared lambda expression

--- Lambda/lambda4-java/src/entities/Product.java
+++ Lambda/lambda4-java/src/entities/Product.java
@@ -25,10 +25,6 @@ public class Product {
 		this.price = price;
 	}
 
-	public String nonStaticUpperCaseName() {
-		return name.toUpperCase();
-	}
-
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
