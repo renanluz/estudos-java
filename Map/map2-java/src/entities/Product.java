65453da4f921068550746966880833d24472a9a3
Renan Luz <ren4n@live.com>
2021-09-29 23:05:10 -0300

    Created hashcode and equals in product class

--- Map/map2-java/src/entities/Product.java
+++ Map/map2-java/src/entities/Product.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Product {
 	private String name;
 	private double price;
@@ -23,6 +25,24 @@ public class Product {
 
 	public void setPrice(double price) {
 		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
 	}
 	
 }
